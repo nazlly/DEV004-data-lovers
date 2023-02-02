0e9d3be6a6ad2729a9096aa76c2180f6a93dd2e1
nazly <nazllydayispalacios22@hotmail.com>
2023-02-01 19:40:56 -0500

    flitrado1

--- src/data.js
+++ src/data.js
@@ -1,13 +1,9 @@
 // estas funciones son de ejemplo
-const discipline =  document.getElementById("discipline").value;
-export const filterSport= (athletes,discipline) => {
-  const resultBySport= athletes.filter(x =>x.sport==discipline);
-  console.log("filtro de desportes", resultBySport);
-  return resultBySport;
-
- 
-};
+export const filtersport = (athletes,discipline) => {
+  const resultBySport= athletes.filter(x=>x.sport===discipline)
+return resultBySport;
+}
 
 export const anotherExample = () => {
-  return 'OMG';          
+  return 'OMG';
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import { filterSport } from './data.js';
+import {filtersport  } from './data.js';
 import dataGames from './data/athletes/athletes.js';
+const athletes=dataGames.athletes;
+const discipline=document.getElementById("discipline");
 
-const discipline =  document.getElementById("discipline").value;
-const result = dataGames.filterSport(discipline);
-    console.log("Resultados", result);
+const tablebody=document.querySelector("tbody");
+const listAthletes=(index)=>{
 
-console.log(filterSport, dataGames);
+    <tr>
+    <td>$(index.name)</td>
+    <td>$(index.sport)</td>
+    </tr>
+    console.log("valor de lista atletas",listAthletes)
+    return("valor de mi table",listAthletes);
+    
+};
+
+const printScreen=(mostrar)=>{
+tablebody.innerHTML="",
+mostrar.forEach((z) => {
+    tablebody.innerHTML +=  listAthletes (z)  
+})
+    
+};
+
+
+console.log(filtersport, dataGames);
