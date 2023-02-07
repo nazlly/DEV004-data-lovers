410c455e3e51a3bb970addb219c048cff0e8a3ba
fperez15 <francis.perez1590@gmail.com>
2023-02-07 08:25:29 -0500

    historia 2 finalizada

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-//filtro de deportes
+// Filtro de deportes
 export const filterSport = (valueSelect, arrayData) => {
   const filterSelect = arrayData.filter(
     (element) => element.sport === valueSelect
@@ -6,19 +6,16 @@ export const filterSport = (valueSelect, arrayData) => {
   return filterSelect;
 };
 
-// Orden alfabetico
-export const sortAlphabetic = (arrayAthletes, arrayOrden) => {
-  if (arrayOrden == "A-Z") {
-    return arrayAthletes.sort((a, b) => {
-      if (a.name < b.name) {
-        return -1;
-      }
-      if (a.name > b.name) {
-        return 1;
-      }
-        return 0;
-      });
-    };
-  };
-
-  
+// Orden alfabético
+export const sortAlphabetic = (arrayFiltrado) => {
+  arrayFiltrado.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+  return arrayFiltrado;
+};
--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@ import { filterSport, sortAlphabetic } from "./data.js";
 import dataGames from "./data/athletes/athletes.js";
 const athletes = dataGames.athletes;
 
+// Variables
 const discipline = document.querySelector("#discipline");
 const tableSport = document.getElementById("tableSport");
+const btnOrdernar = document.getElementById("btnOrdenar");
 
-const createTable = function(arrayDataFiltrado) {
+const createTable = function (arrayDataFiltrado) {
   let stringEncabezado = "<tr><th>Nombre</th><th>Selección</th></tr>";
-  for(let elements of arrayDataFiltrado) {
-    let fila = "<tr>"
+  for (let elements of arrayDataFiltrado) {
+    let fila = "<tr>";
 
-    fila += "<td>"
+    fila += "<td>";
     fila += elements.name;
     fila += "</td>";
 
@@ -23,52 +25,15 @@ const createTable = function(arrayDataFiltrado) {
     stringEncabezado += fila;
   }
   return stringEncabezado;
-}
+};
 
-
-discipline.addEventListener('change', () => {
+discipline.addEventListener("change", () => {
   let valueOption = discipline.value;
-   const filtrandoData = filterSport(valueOption, athletes);
-
+  const filtrandoData = filterSport(valueOption, athletes);
   tableSport.innerHTML = createTable(filtrandoData);
-  
-  const btnOrdenar=document.getElementById("btnOrdenar");
-  btnOrdenar.addEventListener("click",()=> {
-    let ordenarOpcion= discipline.value
-    const sortedArray = sortAlphabetic(ordenarOpcion, athletes);
-    return(tableSport.innerHTML= createTable( sortedArray))   
+
+  btnOrdernar.addEventListener("click", () => {
+    const ordenandoData = sortAlphabetic([...filtrandoData]);
+    tableSport.innerHTML = createTable(ordenandoData);
   });
-
-
 });
-
-
-
-// btnOrdenar.addEventListener("click",()=>{
-  // let ordenarOpcion= ();
-  // const ordenarData= sortAlphabetic(ordenarOpcion,athletes);
-  // tableSport.innerHTML = createTable(ordenarData);
-  
-     //let valueOption = discipline.value;
-    // const filtrandoData = filterSport(valueOption, athletes);
-  // const sortedArray = ;
-  //   console.log(sortedArray);
-  //   return sortedArray;
-     
-    
-  
-
-    
-  
-
-  
-   
-   
-   
-
-
-
- 
-
-
-
